0a5d63e7f76a3feb13b5071b541f71baa30803ca
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-10-14 09:05:09 +0200

    added definition injector wrapper

---
+++ src/Level3/Mongator/Processor/Wrapper/DefinitionInjector.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Level3\Mongator\Processor\Wrapper;
+
+use Mongator\Mongator;
+use Level3\Messages\Request;
+use Level3\Messages\Response;
+use Level3\Processor\Wrapper;
+use Level3\Resource;
+use Closure;
+
+class DefinitionInjector extends Wrapper
+{   
+    protected $mongator;
+
+    public function __construct(Mongator $mongator)
+    {
+        $this->mongator = $mongator;
+    }
+
+    protected function processRequest(Closure $execution, Request $request, $method)
+    {
+        $response = $execution($request);
+
+        $key = $request->getKey();
+        $resource = $response->getResource();
+
+        if (!$key || !$resource || $method == 'error') {
+            return $response;
+        }
+
+        $this->injectDefinitionAsResource($key, $resource);
+
+        return $response;
+    }
+
+    protected function injectDefinitionAsResource($key, Resource $resource)
+    {
+        if (!$key) {
+            return false;
+        }
+
+        $resourceRepository = $this->getLevel3()->getRepository($key);
+        $documentRepository = $resourceRepository->getDocumentRepository();
+
+        $metadata = $documentRepository->getMetadata();
+
+        $definitionResource = new Resource();
+        $definitionResource->setData($metadata);
+
+        $resource->addResource('definition', $definitionResource);
+    }
+}
